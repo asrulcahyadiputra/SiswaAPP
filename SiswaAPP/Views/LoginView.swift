ba23201c9c5a92542a6a0a322d596cc1c5b31ce0
Asrul Cahyadi Putra <asrulcahyadiputra99@gmail.com>
2021-04-02 09:38:08 +0700

    Git Init

---
+++ SiswaAPP/Views/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  SiswaAPP
+//
+//  Created by Asrul Cahyadi Putra on 02/04/21.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Login View")
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
