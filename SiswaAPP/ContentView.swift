c44bdd460f3f6a03c4a419d2d21e8e1d8b601913
Asrul Cahyadi Putra <asrulcahyadiputra99@gmail.com>
2021-04-02 09:30:03 +0700

    Initial Commit

---
+++ SiswaAPP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SiswaAPP
+//
+//  Created by Asrul Cahyadi Putra on 02/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
