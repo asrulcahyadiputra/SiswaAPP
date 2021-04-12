37878d8ee15c45e71a19669b403f7bce4cc92554
Asrul Cahyadi Putra <asrulcahyadiputra99@gmail.com>
2021-04-12 18:28:16 +0700

    Navigation View at Home View

--- SiswaAPP/Views/HomeView.swift
+++ SiswaAPP/Views/HomeView.swift
@@ -27,96 +27,100 @@ struct Home : View {
     @State var index = 0
     
     var body: some View{
-        
-        VStack(spacing: 0){
-            ZStack{
-                VStack{
-                    
-                    HStack{
+        NavigationView{
+            VStack(spacing: 0){
+                ZStack{
+                    VStack{
                         
-                        Button(action: {
+                        HStack{
                             
-                        }) {
-                            
-                            Image("user")
-                                .renderingMode(.original)
-                                .resizable()
-                                .frame(width: 50, height: 50)
-                        }
-                        
-                        VStack(alignment:.leading){
-                            Text(userAuth.name)
-                                .font(.title2)
-                                .fontWeight(.bold)
-                            Text("Kelas " + userAuth.kodeKelas)
-                                .font(.system(size: 14))
-                                .fontWeight(.semibold)
-                        }
-                        
-                        Spacer()
-                       
-                        
-                    }
-                    .padding(.horizontal)
-                    .padding(.top,70)
-                    .padding(.bottom, 50)
-                }
-              
-            }
-            .background(Color.white)
-           
-           
-            ZStack{
-                
-                ScrollView(.vertical, showsIndicators: false){
-                    
-                    
-                    ScrollView(.vertical, showsIndicators: false, content:{
-                        VStack(spacing: 20){
-                            ScrollView {
-                                LazyVGrid(columns: [
-                                    GridItem(.flexible(minimum: 100, maximum: 200), spacing: 12, alignment: .top),
-                                    GridItem(.flexible(minimum: 100, maximum: 200), spacing: 12, alignment: .top),
-                                    GridItem(.flexible(minimum: 100, maximum: 200), spacing: 12, alignment: .top),
-                                ], spacing: 12, content: {
-                                    //list mapel here
-                                  
-                                        ForEach(mp.mapels){ ls in
-                                         
-                                            VStack(alignment: .center){
-                                                Spacer()
-                                                Image(ls.kodeMatpel)
-                                                    .resizable()
-                                                    .frame(width: 50, height: 50)
-                                                    .shadow(radius: 0.3)
-                                                    .padding()
-                                                Text(ls.singkatan)
-                                                    .font(.system(size: 14))
-                                            }
-                                            .padding(.horizontal)
-                                        }
-                                    
-                                  
-                                    
-                                })
-                                .padding(.horizontal,12)
+                            Button(action: {
+                                
+                            }) {
+                                
+                                Image("user")
+                                    .renderingMode(.original)
+                                    .resizable()
+                                    .frame(width: 50, height: 50)
                             }
+                            
+                            VStack(alignment:.leading){
+                                Text(userAuth.name)
+                                    .font(.title2)
+                                    .fontWeight(.bold)
+                                Text("Kelas " + userAuth.kodeKelas)
+                                    .font(.system(size: 14))
+                                    .fontWeight(.semibold)
+                            }
+                            
+                            Spacer()
+                           
+                            
                         }
                         .padding(.horizontal)
-                        .padding(.top,10)
-                        .padding(.bottom)
-                    })
-                    .padding(.bottom,60)
-                    
-                    
+                        .padding(.top,70)
+                        .padding(.bottom, 50)
+                    }
+                  
                 }
                 .background(Color.white)
+               
+               
+                ZStack{
+                    
+                    ScrollView(.vertical, showsIndicators: false){
+                        ScrollView(.vertical, showsIndicators: false, content:{
+                            VStack(spacing: 20){
+                                ScrollView {
+                                    LazyVGrid(columns: [
+                                        GridItem(.flexible(minimum: 100, maximum: 200), spacing: 12, alignment: .top),
+                                        GridItem(.flexible(minimum: 100, maximum: 200), spacing: 12, alignment: .top),
+                                        GridItem(.flexible(minimum: 100, maximum: 200), spacing: 12, alignment: .top),
+                                    ], spacing: 12, content: {
+                                        //list mapel here
+                                      
+                                        ForEach(mp.mapels){ ls in
+                                            NavigationLink( destination: PelajaranDetailView()){
+                                                VStack(alignment: .center){
+                                                 
+                                                    Image(ls.kodeMatpel)
+                                                        .resizable()
+                                                        .frame(width: 50, height: 50)
+                                                        .shadow(radius: 0.3)
+                                                        .padding()
+                                                    Text(ls.singkatan)
+                                                        .font(.system(size: 14))
+                                                }
+                                                .padding(.horizontal)
+                                            }
+                                            .buttonStyle(PlainButtonStyle())
+                                            
+                                        }
+                                        
+                                      
+                                        
+                                    })
+                                    .padding(.horizontal,12)
+                                }
+                            }
+                            .padding(.horizontal)
+                            .padding(.top,10)
+                            .padding(.bottom)
+                        })
+                        .padding(.bottom,60)
+                        
+                        
+                    }
+                    .background(Color.white)
+                }
+                Spacer()
             }
-            Spacer()
+            .edgesIgnoringSafeArea(.all)
+            .background(Color("bg-dark"))
+            .statusBar(hidden: true)
         }
-        .edgesIgnoringSafeArea(.all)
-        .background(Color("bg-dark"))
-        .statusBar(hidden: true)
+        
+       
     }
 }
 
---
+++ SiswaAPP/Views/PelajaranDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  PelajaranDetailView.swift
+//  SiswaAPP
+//
+//  Created by Asrul Cahyadi Putra on 12/04/21.
+//
+
+import SwiftUI
+
+struct PelajaranDetailView: View {
+    var body: some View {
+        Text("Detail Mata Pelajaran")
+    }
+}
+
+struct PelajaranDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        PelajaranDetailView()
+    }
+}
