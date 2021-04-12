c1bd27d6f31f3842e07b0d1bca94adb581954c53
Asrul Cahyadi Putra <asrulcahyadiputra99@gmail.com>
2021-04-12 20:49:09 +0700

    detail pelajaran view

--- SiswaAPP/Views/PelajaranDetailView.swift
+++ SiswaAPP/Views/PelajaranDetailView.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct PelajaranDetailView: View {
+//    @ObservedObject var mp = MapelController()
+  
     var body: some View {
-        Text("Detail Mata Pelajaran")
+        VStack{
+            HStack {
+                VStack(alignment: .leading, spacing:15){
+                    Text("Hai")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    Text("Selamat Pagi!")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    
+                }
+                Spacer()
+            }
+            .padding(.leading,70)
+            
+        }
+        .background(RadialGradient(gradient: Gradient(colors: [Color("dark-blue"), Color.blue]), center: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, startRadius: 5, endRadius: 500))  
     }
 }
 
