0281615afc9f156c66f85802769d9d6a5e5e9098
asrulcahyadiputra <asrulcahyadiputra99@gmail.com>
2021-04-16 09:41:26 +0700

    Akun Top Bar

--- SiswaAPP/Views/AkunView.swift
+++ SiswaAPP/Views/AkunView.swift
@@ -9,9 +9,41 @@ import SwiftUI
 
 struct AkunView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       AkunTop()
     }
 }
+
+//MARK: -Top Bar Akun
+struct AkunTop: View {
+    var  body: some View {
+        VStack{
+            VStack{
+                Text("Profil")
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+            }
+            .padding(.top,30)
+            
+            VStack(spacing: 15){
+                Image("user")
+                    .resizable()
+                    .frame(width: 76, height: 76)
+                
+                Text("Nama Akun Here")
+                    .foregroundColor(Color("dark-blue"))
+                    .font(.system(size: 14, weight: .bold))
+                
+                Text("123456")
+                    .font(.system(size: 12))
+            }
+          
+            Spacer()
+        }
+    }
+}
+
+//MARK: -Body Akun View
+
 
 struct AkunView_Previews: PreviewProvider {
     static var previews: some View {
