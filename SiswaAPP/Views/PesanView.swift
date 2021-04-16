258bc83b1be09615cdb436f87f7595d6fb5e2bc2
asrulcahyadiputra <asrulcahyadiputra99@gmail.com>
2021-04-17 01:26:13 +0700

    Pesan View Navigation

--- SiswaAPP/Views/PesanView.swift
+++ SiswaAPP/Views/PesanView.swift
@@ -8,13 +8,88 @@
 import SwiftUI
 
 struct PesanView: View {
+    @State var selectedPesan = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            VStack{
+                VStack{
+                    PesanTopBar(selected: $selectedPesan)
+                    GeometryReader{ _ in
+                        VStack{
+                            if self.selectedPesan == 0 {
+                               InboxView()
+                            }
+                            else if self.selectedPesan == 1 {
+                              NotifikasiView()
+                                
+                            }
+                        }
+                        Spacer()
+                    }
+                }
+                Spacer()
+            }
+        }
+        .ignoresSafeArea(.all, edges: .all)
+        Spacer()
     }
 }
 
 struct PesanView_Previews: PreviewProvider {
     static var previews: some View {
         PesanView()
+    }
+}
+
+
+struct PesanTopBar: View {
+    @Binding var selected: Int
+    var body: some View{
+        ZStack{
+            VStack{
+                HStack{
+                    Button(action: {
+                        self.selected = 0
+                    }){
+                        HStack{
+                            Image(systemName: "message")
+                                .foregroundColor(self.selected == 0 ? Color("dark-blue") : .black)
+                            Text("Inbox")
+                                .foregroundColor(self.selected == 0 ? Color("dark-blue") : .black)
+                        }
+                    }
+                    Spacer()
+                    Button(action: {
+                        self.selected = 1
+                    }){
+                        HStack{
+                            Image(systemName: "bell")
+                                .foregroundColor(self.selected == 1 ? Color("dark-blue") : .black)
+                            Text("Notifikasi")
+                                .foregroundColor(self.selected == 1 ? Color("dark-blue") : .black)
+                        }
+                    }
+                }
+                .padding(.top,80)
+                .padding(.leading,80)
+                .padding(.trailing,80)
+                .padding(.bottom,20)
+            }
+            .background(Color.white)
+            .shadow(color: Color("bg-dark"), radius: 10, x: 0.0, y: 0.0)
+            Spacer()
+        }
+    }
+}
+
+struct InboxView: View {
+    var body: some View{
+        Text("Inbox")
+    }
+}
+
+struct NotifikasiView: View {
+    var body: some View{
+        Text("Notifikasi")
     }
 }
