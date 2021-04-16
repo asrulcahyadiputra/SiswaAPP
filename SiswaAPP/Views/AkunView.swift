32b4c6fbf118a9c6b8f90a05295a92fd08759aff
asrulcahyadiputra <asrulcahyadiputra99@gmail.com>
2021-04-16 09:03:27 +0700

    Create a new interface file AkunView

---
+++ SiswaAPP/Views/AkunView.swift
@@ -0,0 +1,20 @@
+//
+//  AkunView.swift
+//  SiswaAPP
+//
+//  Created by Asrul Cahyadi Putra on 16/04/21.
+//
+
+import SwiftUI
+
+struct AkunView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AkunView_Previews: PreviewProvider {
+    static var previews: some View {
+        AkunView()
+    }
+}
