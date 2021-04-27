c461d822083eb49b25c538f87aa56b6753dbfc7a
asrulcahyadiputra <asrulcahyadiputra99@gmail.com>
2021-04-28 01:14:55 +0800

    Fixed UI notification view

--- SiswaAPP/Views/NotifikasiView.swift
+++ SiswaAPP/Views/NotifikasiView.swift
@@ -13,46 +13,67 @@ struct NotifikasiView: View {
         VStack{
             VStack{
                 List{
-                    HStack{
-                        VStack{
-                            HStack{
-                                Image("fa-notif")
-                                    .resizable()
-                                    .frame(width: 10, height: 10)
-                                Text("Pemberitahuan")
-                                    .foregroundColor(.gray)
-                                    .font(.system(size: 12))
-                                Text("25-03-2021")
-                                    .foregroundColor(.gray)
-                                    .font(.system(size: 12))
-                                Spacer()
-                            }
-                            .padding(.bottom,10)
-                            VStack{
-                                HStack{
-                                    Text("Lorem ipsum dolor sit amet.")
-                                        .fontWeight(.bold)
-                                        .font(.system(size: 14))
-                                    Spacer()
-                                }
-                                .padding(.bottom,10)
-                                VStack{
-                                    Text("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam....")
-                                        .font(.system(size: 14))
-                                }
-                                
-                            }
-                            
+                    VStack{
+                        HStack{
+                            Image("fa-notif")
+                                .resizable()
+                                .frame(width: 10, height: 10)
+                            Text("Pemberitahuan")
+                                .foregroundColor(.gray)
+                                .font(.system(size: 12))
+                            Text("25-03-2021")
+                                .foregroundColor(.gray)
+                                .font(.system(size: 12))
+                            Spacer()
                         }
-                        
-                        
+                        .padding(.bottom,10)
+                        HStack{
+                            Text("Lorem ipsum dolor sit amet.")
+                                .fontWeight(.bold)
+                                .font(.system(size: 14))
+                            Spacer()
+                        }
+                        .padding(.bottom,10)
+                        HStack{
+                            Text("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam....")
+                                .font(.system(size: 14))
+                            Spacer()
+                        }
                     }
-                    
+                    .listRowBackground(self.readNotification == false ? Color("unread-message") : Color.white)
+                    .padding(.top)
+                    VStack{
+                        HStack{
+                            Image("fa-notif")
+                                .resizable()
+                                .frame(width: 10, height: 10)
+                            Text("Pemberitahuan")
+                                .foregroundColor(.gray)
+                                .font(.system(size: 12))
+                            Text("25-03-2021")
+                                .foregroundColor(.gray)
+                                .font(.system(size: 12))
+                            Spacer()
+                        }
+                        .padding(.bottom,10)
+                        HStack{
+                            Text("Lorem ipsum dolor sit amet.")
+                                .fontWeight(.bold)
+                                .font(.system(size: 14))
+                            Spacer()
+                        }
+                        .padding(.bottom,10)
+                        HStack{
+                            Text("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam....")
+                                .font(.system(size: 14))
+                            Spacer()
+                        }
+                    }
+                    .listRowBackground(self.readNotification == false ? Color("unread-message") : Color.white)
+                    .padding(.top)
                 }
                 
             }
-            .background(Color("unread-message"))
-            
         }
     }
     
